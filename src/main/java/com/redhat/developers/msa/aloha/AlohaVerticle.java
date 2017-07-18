ac30b3fce79fb8533c6fe41a5f669d84ec69d4e3
jaig1 <njganeshv@gmail.com>
2017-07-18 18:28:40 -0500

    Test Commit

--- src/main/java/com/redhat/developers/msa/aloha/AlohaVerticle.java
+++ src/main/java/com/redhat/developers/msa/aloha/AlohaVerticle.java
@@ -101,7 +101,7 @@ public class AlohaVerticle extends AbstractVerticle {
 
     private String aloha() {
         String hostname = System.getenv().getOrDefault("HOSTNAME", "unknown");
-        return String.format("Aloha mai Jai from %s", hostname);
+        return String.format("Aloha mai Kiran from %s", hostname);
         
     }
 
